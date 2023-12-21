cf9016749edb1131f07eab6ab525dd5466fbeb15
MTahirSB <107554827+MTahirSB@users.noreply.github.com>
2023-12-21 20:56:24 +0530

    Update openaiclient.js

--- src/openai/openaiclient.js
+++ src/openai/openaiclient.js
@@ -1,7 +1,7 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-  apiKey: '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', // This is the default and can be omitted
+  apiKey: '', // This is the default and can be omitted
   dangerouslyAllowBrowser: true
 });
 
