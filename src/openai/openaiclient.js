61a96e1af8b2741fe6c74d45ec0d4c094d9566ea
MTahirSB <107554827+MTahirSB@users.noreply.github.com>
2023-12-21 20:57:35 +0530

    Update openaiclient.js

--- src/openai/openaiclient.js
+++ src/openai/openaiclient.js
@@ -7,7 +7,7 @@ const openai = new OpenAI({
 
 export default async function respond(messages) {
   const chatCompletion = await openai.chat.completions.create({
-    messages: [{ role: 'system', content: 'You are a very helpful assistant ,You try to help users in any way possible , remember not to suggest anything'},
+    messages: [{ role: 'system', content: ''},
     { role: 'user', content: messages}],
     model: 'gpt-3.5-turbo',
   }).then(res=> res.choices[0].message.content)
